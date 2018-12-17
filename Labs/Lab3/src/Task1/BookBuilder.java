e493b23ce2642fc9ab61b9f55c8f91d2bc4f1083
kirick1 <kyrychenko.vladyslav@gmail.com>
2018-12-17 02:25:06 +0200

    fix lab3

--- Labs/Lab3/src/Task1/BookBuilder.java
+++ Labs/Lab3/src/Task1/BookBuilder.java
@@ -3,12 +3,12 @@ package Task1;
 import сommon.EntityList;
 
 public interface BookBuilder {
-    void setId(int id);
-    void setTitle(String title);
-    void setYear(int year);
-    void setPages(int pages);
-    void setPrice(double price);
-    void setBinding(Binding binding);
-    void setPublisher(Publisher publisher);
-    void setAuthors(EntityList<Author> authors);
+  void setId(int id);
+  void setTitle(String title);
+  void setYear(int year);
+  void setPages(int pages);
+  void setPrice(double price);
+  void setBinding(Binding binding);
+  void setPublisher(Publisher publisher);
+  void setAuthors(EntityList<Author> authors);
 }
